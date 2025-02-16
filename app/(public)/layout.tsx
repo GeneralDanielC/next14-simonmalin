990cfa71cbb285d4cc592e0d50d383977e467d0d
GeneralDanielC <danielcarlsson2002@gmail.com>
2025-02-16 16:18:51 +0100

    Add maintenance mode. Maintenance mode active.

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -10,6 +10,7 @@ import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import Link from "next/link";
 import { NavBar } from "./_components/navbar";
 import { Toaster } from "sonner";
+import { ContactWedding } from "@/components/contact-wedding";
 
 const cormorant = Cormorant_SC({ weight: ["300", "400", "500", "600", "700"], subsets: ["latin"] });
 
@@ -29,6 +30,8 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const maintenance = true;
+
   return (
     <html lang="sv">
       <head>
@@ -37,7 +40,17 @@ export default function RootLayout({
       <body className={cn(cormorant.className, "font-light bg-beige min-h-screen flex flex-col")}>
         <NavBar />
         <main className="flex-grow">
-          {children}
+          {maintenance ? (
+            <div className="h-screen w-full flex flex-col justify-center items-center p-24 text-center">
+              <h1 className="text-4xl">Underhåll!</h1>
+              <h2>Just nu utför vi ett planerat underhåll för att göra er upplevelse ännu bättre. Kika igen lite senare.</h2>
+              <div className="rounded-xl border border-black p-5 mt-10 flex flex-col">
+                <p>Vid frågor</p>
+                <a href={`mailto:daniel.carlsson@noll2.io`} className="underline">daniel.carlsson@noll2.io</a>
+                <ContactWedding text={`${process.env.NEXT_PUBLIC_SUPPORT_EMAIL}`} />
+              </div>
+            </div>
+          ) : children}
         </main>
         <Toaster
           toastOptions={{
